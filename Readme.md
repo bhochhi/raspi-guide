7d72ea4202bea2c853caf8929bb2ecf94effb8b5
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-07-31 02:13:38 -0500

    gpio

--- Readme.md
+++ Readme.md
@@ -4,4 +4,5 @@
 * [WIFI setup](https://learn.adafruit.com/adafruits-raspberry-pi-lesson-3-network-setup/setting-up-wifi-with-occidentalis)  
 * [Creating Z wave smart-home](https://raspberrypihq.com/how-to-create-a-z-wave-smart-home-hub-using-a-raspberry-pi/)
 * [Bread Board](https://learn.sparkfun.com/tutorials/how-to-use-a-breadboard)
-* [IntelliJ/Pycharm with raspberry pi deployment. Remote debugging](https://github.com/Multibit-Legacy/trezorj/wiki/Configuring-Intellij-to-use-Python-and-deploy-to-the-Raspberry-Pi)  
+* [IntelliJ/Pycharm with raspberry pi deployment.](https://github.com/Multibit-Legacy/trezorj/wiki/Configuring-Intellij-to-use-Python-and-deploy-to-the-Raspberry-Pi)
+* []
--- gpio/blink.py
+++ gpio/blink.py
@@ -1,23 +1,37 @@
+# External module imports
 import RPi.GPIO as GPIO
 import time
 
-# Run `gpio readall` to confirm BCM pin number(s)
-pinOut = 17	# LED
+# Pin Definitons:
+pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
+ledPin = 23 # Broadcom pin 23 (P1 pin 16)
+butPin = 17 # Broadcom pin 17 (P1 pin 11)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(pinOut, GPIO.OUT)
+dc = 95 # duty cycle (0-100) for PWM pin
 
-def blink(pin):
-  GPIO.output(pin,GPIO.HIGH)
-  time.sleep(0.5)
-  GPIO.output(pin,GPIO.LOW)
-  time.sleep(0.5)
-  return
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
+pwm = GPIO.PWM(pwmPin, 40)  # Initialize PWM on pwmPin 100Hz frequency
+GPIO.setup(butPin, GPIO., pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
+# Initial state for LEDs:
+GPIO.output(ledPin, GPIO.LOW)
+pwm.start(dc)
+
+print("Here we go! Press CTRL+C to exit")
 try:
-  for i in range(0,5):
-    blink(pinOut)
-finally:
-  GPIO.cleanup()
-  print("\nDone")
+    while 1:
+        if GPIO.input(butPin): # button is released
+            pwm.ChangeDutyCycle(dc)
+            GPIO.output(ledPin, GPIO.LOW)
+        else: # button is pressed:
+            pwm.ChangeDutyCycle(100-dc)
+            GPIO.output(ledPin, GPIO.HIGH)
+            time.sleep(0.075)
+            GPIO.output(ledPin, GPIO.LOW)
+            time.sleep(0.075)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    pwm.stop() # stop PWM
+    GPIO.cleanup() # cleanup all GPIO
